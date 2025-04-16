ab3fde665df85b5a46aa54d839d7b6f55b2bd493
Luka Alimbarashvili <lalimbarashvili@devexperts.com>
2025-04-16 17:17:44 +0400

    Initial commit

---
+++ fastlaneTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fastlaneTest
+//
+//  Created by Luka Alimbarashvili on 16.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fastlaneTestTests/fastlaneTestTests.swift
@@ -0,0 +1,22 @@
+//
+//  fastlaneTestTests.swift
+//  fastlaneTestTests
+//
+//  Created by Luka Alimbarashvili on 16.04.25.
+//
+
+import Testing
+import XCTest
+@testable import fastlaneTest
+
+struct fastlaneTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test func testAssertEqual() {
+        XCTAssertTrue(1 == 1)
+    }
+
+}
